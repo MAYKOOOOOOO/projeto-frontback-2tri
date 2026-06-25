3c6d3eded3b23a5d06db0c8e3266fc913f700dd2
MAYKOOOOO <maykon.salvador@escola.pr.gov.br>
2026-06-25 09:42:31 -0300

    terminei as atv 10 de backt

---
+++ atv10.back/script.js
@@ -0,0 +1,5 @@
+console.log('maykon');
+console.log('projeto-frontback-2tri');
+console.log('mensagem1');
+console.log('mensagem2');
+console.log('mensagem3');
---
+++ atv10.back/testesimple.js
@@ -0,0 +1,9 @@
+const resultado = 2 + 2;
+
+if (resultado === 4) {
+    console.log("Sucesso!");
+    console.log('seu calcula esta correto')
+} else {
+    console.log("Erro");
+    console.log('seu calcula esta errado')
+}
